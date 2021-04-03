f51fe1741cfc16f6c05e6d9f94a6fee81ad8fe4f
Le Xuan Du <dulx96@gmail.com>
2021-04-03 13:03:04 +0900

    [add] oke

--- browser/main.py
+++ browser/main.py
@@ -3,6 +3,8 @@ from playwright.async_api import async_playwright, Page
 from aiorun import run
 import yaml
 import os
+from dotenv import load_dotenv
+import logging
 
 async def login(page:Page, username:str, password:str):
     await page.fill('input[name=email]', username)
@@ -10,23 +12,62 @@ async def login(page:Page, username:str, password:str):
     await page.keyboard.press('Enter')
     await page.wait_for_load_state('networkidle')
     await asyncio.sleep(10)
-
+    page.wait_for_selector("[data-list-id='chat-messages']")
+    print('login success')
 
 
 async def main():
     # * load config
-    with open('config.yaml','r') as f:
+    load_dotenv()
+    logging.info('start')
+    with open(os.environ.get('CONFIG_FILE'),'r') as f:
         config = yaml.load(f)
         print(config)
     # * setup browser
     playwright = await async_playwright().start()
-    browser_context = await playwright.chromium.launch(headless=False, args=[
-                '--disable-web-security'
+    browser = await playwright.chromium.launch(headless=True, args=[
+                '--autoplay-policy=user-gesture-required',
+    '--disable-background-networking',
+    '--disable-background-timer-throttling',
+    '--disable-backgrounding-occluded-windows',
+    '--disable-breakpad',
+    '--disable-client-side-phishing-detection',
+    '--disable-component-update',
+    '--disable-default-apps',
+    '--disable-dev-shm-usage',
+    '--disable-domain-reliability',
+    '--disable-extensions',
+    '--disable-features=AudioServiceOutOfProcess',
+    '--disable-hang-monitor',
+    '--disable-ipc-flooding-protection',
+    '--disable-notifications',
+    '--disable-offer-store-unmasked-wallet-cards',
+    '--disable-popup-blocking',
+    '--disable-print-preview',
+    '--disable-prompt-on-repost',
+    '--disable-renderer-backgrounding',
+    '--disable-setuid-sandbox',
+    '--disable-speech-api',
+    '--disable-sync',
+    '--disk-cache-size=33554432',
+    '--hide-scrollbars',
+    '--ignore-gpu-blacklist',
+    '--metrics-recording-only',
+    '--mute-audio',
+    '--no-default-browser-check',
+    '--no-first-run',
+    '--no-pings',
+    '--no-sandbox',
+    '--no-zygote',
+    '--password-store=basic',
+    '--use-gl=swiftshader',
+    '--use-mock-keychain',
+                '--disable-blink-features=AutomationControlled'
             ],
             proxy={'server': os.environ['PROXY'],
                    'username': os.environ['PROXY_USER'],
                    'password': os.environ['PROXY_PASS']} if os.environ.get('USE_PROXY', 'false') == 'true' else None)
-
+    browser_context = await browser.new_context(user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36')
     # * process each profile
     for item in config['pages'].values():
         page = await browser_context.new_page()
